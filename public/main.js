28b7d06218bbcbecac964e3e5138a54697d73755
petamoriken <suteaka_sayonara@yahoo.co.jp>
2015-01-27 18:30:01 +0900

    mouse.length test

--- public/main.js
+++ public/main.js
@@ -53,16 +53,14 @@
 
 		requestAnimationFrame(drawMouse);
 		function drawMouse() {
-			var num = 0;
 			ctx.clearRect(0, 0, width, height);
 			mouse.forEach(function(data) {
 				if(data.id !== myId)
 					ctx.beginPath();
 					ctx.rect(data.x, data.y, 10, 10);
 					ctx.stroke();
-					++num;
 			});
-			console.log(num);
+			console.log(mouse.length);
 			requestAnimationFrame(drawMouse);
 		}
 
