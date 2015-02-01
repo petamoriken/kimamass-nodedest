90553ae851ec87f8c42c4dfce2a9a58ef945aeb2
petamoriken <suteaka_sayonara@yahoo.co.jp>
2015-02-02 06:04:29 +0900

    fix

--- public/main.js
+++ public/main.js
@@ -130,8 +130,10 @@
 
 				if(opening <= 10) {
 					ctx.fillStyle = "rgba(255, 255, 255, 0.05)";
-				} else {
+				} else if(opening <= 35) {
 					ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+				} else {
+					ctx.fillStyle = "#fff";
 				}
 				ctx.fillRect(0, 0, width, height);
 
